73a4fe90c39885e7f978162dda60410f6a7fc28e
FredrikKii <f.eriksson@mail.com>
2024-09-02 12:40:30 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Köp fiskeaffären.se nu!</h1>
+	  <p>f.eriksson@mail.com</p>
     </>
   )
 }
